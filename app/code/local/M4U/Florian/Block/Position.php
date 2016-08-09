df99845e82204e84b278f184ec326c2ed7fbc0e6
Florian <f.ceprika@gmail.com>
2016-08-09 14:08:29 +0100

    display product list

--- app/code/local/M4U/Florian/Block/Position.php
+++ app/code/local/M4U/Florian/Block/Position.php
@@ -1,4 +1,19 @@
-<?php class M4U_Florian_Block_Position extends Mage_Core_Block_Template
+<?php class M4U_Florian_Block_Position extends Mage_Catalog_Block_Product_Abstract
 {
-    // necessary methods
+
+    protected $_params;
+
+    public function _getProductCollection()
+    {
+        $this->_params = $this->getRequest()->getParams();
+        Mage::log($this->_params,null,'florian.log');
+
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('cat_position', array('nlike' => ''))
+            ->addAttributeToSort('cat_position', 'ASC');
+
+
+        return $collection;
+    }
 }
